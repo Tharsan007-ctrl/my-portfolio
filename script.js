51dfdfdca9e60a7d08a7f79240f6dbbb05772d18
Tharsan007-ctrl <tharshanbpt007@gmail.com>
2025-03-09 01:10:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent Page Refresh
+
+    // Get Form Values
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+    // Simple Validation
+    if (name === "" || email === "" || message === "") {
+        alert("எல்லா Box-ஐயும் நிரப்பவும்!");
+        return;
+    }
+
+    // Show Success Message
+    document.getElementById("responseMessage").style.display = "block";
+
+    // Clear Form Fields
+    document.getElementById("contactForm").reset();
+});
